3e8d96bab5a9816808f40a5e9f37674a9b6e8056
yuvraj <yuvraj.gangwar@daffodilsw.com>
2024-11-12 13:19:54 +0530

    axios wrapper

---
+++ AxiosInstance.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const createAxiosInstance = (BASE_URL) => {
+  const commonAxios = axios.create({
+    baseURL: BASE_URL,
+  });
+
+  // Optional: Add an interceptor for Authorization headers
+//   commonAxios.interceptors.request.use(
+    // (config) => {
+    //   const token = localStorage.getItem('token'); // Example for token retrieval
+    //   if (token) {
+    //     config.headers["Authorization"] = `Bearer ${token}`;
+    //   }
+    //   return config;
+    // },
+    // (error) => {
+    //   return Promise.reject(error);
+    // }
+//   );
+
+  return commonAxios;
+};
+
+export default createAxiosInstance;
